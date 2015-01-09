91be3fb2f1c323990501da02afb99f3b66a45e3b
jjureta <josip.jureta@gmail.com>
2015-01-08 22:56:40 -0500

    change code comments

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 ## This scripts downloads, if not already done, data set from UCI site, 
 ## loads data from the ./data/ directory and plots Global Active Power
-## histogram. Generated histogram is saved in ./plots/plot1.png file
+## histogram. Generated histogram is saved in ./plot1.png file
 
 library(dplyr)
 library(data.table)
@@ -11,7 +11,6 @@ source("./tools.R")
 setwd("E:/doc/projects/r-test/ExData_Plotting1")
 
 dataFolder <- "./data"
-
 fileName <- "household_power_consumption.txt"
 
 fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
@@ -20,7 +19,8 @@ filePath <- paste(dataFolder, fileName, sep = "/")
 ## download UCI data (see tools.R)
 downloadData(dataFolder)
 ## subset UCI data for given range (see tools.R)
-subsetFilePath <- subsetData(as.Date("2007-02-01"), as.Date("2007-02-02"), dataFolder, filePath)
+subsetFilePath <- subsetData(as.Date("2007-02-01"), as.Date("2007-02-02"), 
+                             dataFolder, filePath)
 
 ## load data from the file
 powerConsumption <- fread(subsetFilePath, na.strings = "?")
